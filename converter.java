2dbff69788ccfac9564f6a61ffb3751b46f84423
Lakshya Kaira <lakshyasinghkaira341@gmail.com>
2026-03-15 00:11:08 +0530

    Add files via upload

---
+++ converter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class converter {
+    static void main() {
+        System.out.println("km to mm converter");
+        System.out.println("Enter your number below (not write kilometer,only number) which you want to convert into mili meter");
+        Scanner ab=new Scanner(System.in);
+        int a=ab.nextInt();
+        int mm=a*1000000;
+        System.out.println(a+"km = "+mm+"mm");
+
+    }
+}
